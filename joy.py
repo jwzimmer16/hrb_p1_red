b865a1ebf652acd28d5b81eaefe44bc6ae8e1836
Joe Zimmer <jwzimmer@umich.edu>
2017-02-13 14:06:13 -0500

    Removed unnecessary files

--- joy.py
+++
@@ -1,7 +0,0 @@
-import joy
-"""
-The JoyApp framework has moved to its own package (subdirectory)
-
-To maintain backward compatibility, this stub calls the unit test for joy
-"""
-joy.test()
--- joy/decl.py
+++
@@ -1,8 +0,0 @@
-from pygix.constants import *
-
-from pygix import (
-  TIMEREVENT, CKBOTPOSITION, SCRATCHUPDATE, MIDIEVENT
-  )
-  
-from loggit import progress
-
